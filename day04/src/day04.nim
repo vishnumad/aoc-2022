10cae50f7bca3de4dc0ccc98f038c0bea0b9ed23
Vishnu <3137711+vishnumad@users.noreply.github.com>
2022-12-04 17:52:53 -0600

    Day 4 cleanup

--- day04/src/day04.nim
+++ day04/src/day04.nim
@@ -3,20 +3,17 @@ import std/[strutils, sequtils]
 type
   Range = tuple[low, high: int]
 
-proc parseRange(literal: string): Range =
-  let range = literal.split('-')
-  return (low: parseInt(range[0]), high: parseInt(range[1]))
-
 proc length(range: Range): int =
   return range.high - range.low + 1
 
-proc span(a: Range, b: Range): Range =
-  return (low: min(a.low, b.low), high: max(a.high, b.high))
-
 proc overlap(a: Range, b: Range): int =
-  let spanLength = span(a, b).length
+  let spanLength = max(a.high, b.high) - min(a.low, b.low) + 1
   let totalLength = a.length + b.length
   return totalLength - spanLength
+
+proc parseRange(literal: string): Range =
+  let range = literal.split('-')
+  return (low: parseInt(range[0]), high: parseInt(range[1]))
 
 let ranges = readFile("input")
   .strip()
@@ -27,8 +24,7 @@ let ranges = readFile("input")
 block part1:
   var count = 0
   for (a, b) in ranges:
-    let overlap = overlap(a, b)
-    if overlap >= a.length or overlap >= b.length:
+    if overlap(a, b) == min(a.length, b.length):
       count += 1
 
   echo "Part 1: ", count
@@ -40,4 +36,3 @@ block part2:
       count += 1
 
   echo "Part 2: ", count
-
